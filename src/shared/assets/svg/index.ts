b5200f7155df4c1691260485431fae85c1e7b9d6
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-17 16:04:11 +0900

    feat: Add new SVG icons for Home, Folder, Blog, and Mail, and export them from the index.ts file

---
+++ src/shared/assets/svg/index.ts
@@ -0,0 +1,4 @@
+export { default as HomeIcon } from './home-icon.svg'
+export { default as ProjectIcon } from './folder-icon.svg'
+export { default as PostIcon } from './blog-icon.svg'
+export { default as MailIcon } from './mail-icon.svg'
