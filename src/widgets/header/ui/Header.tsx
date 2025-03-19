5f538749330d8dde96f0fde6480c1afa1be68b23
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-19 16:54:32 +0900

    refactor: Update Header component to use centralized SVG asset imports and enhance link accessibility by use cursor pointer

--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -2,14 +2,14 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import { HomeIcon, ProjectIcon, PostIcon, MailIcon } from '@/shared/assets/svg'
+import { HomeIcon, ProjectIcon, PostIcon, MailIcon } from '@/shared/assets'
 
 export const Header = () => {
   const pathname = usePathname()
 
   return (
     <header className="flex items-center justify-between px-8 py-4 bg-bg-primary/80 backdrop-blur-sm">
-      <Link href="/" className="text-xl font-semibold" aria-label="Go to home">
+      <Link href="/" className="text-xl font-semibold cursor-pointer" aria-label="Go to home">
         <div className="relative flex items-center gap-2 border border-slate-800 rounded-3xl px-4 py-2">
           <div className="absolute w-3 h-3 bg-green-400 rounded-full animate-ping"></div>
           <div className="w-3 h-3 bg-green-400 rounded-full"></div>
@@ -20,7 +20,7 @@ export const Header = () => {
       <nav className="flex gap-4 font-semibold border border-slate-800 rounded-3xl px-3 py-2">
         <Link href="/" aria-label="Go to home page">
           <div
-            className={`flex items-center gap-1 ${pathname === '/' ? 'text-white' : 'text-slate-400'}`}
+            className={`flex items-center gap-1 cursor-pointer ${pathname === '/' ? 'text-white' : 'text-slate-400'}`}
           >
             <HomeIcon
               className={`w-5 h-5 ${pathname === '/' ? 'stroke-white' : 'stroke-slate-400'}`}
@@ -31,7 +31,7 @@ export const Header = () => {
         </Link>
         <Link href="/projects" aria-label="Go to my projects page">
           <div
-            className={`flex items-center gap-1 ${pathname === '/projects' ? 'text-white' : 'text-slate-400'}`}
+            className={`flex items-center gap-1 cursor-pointer ${pathname === '/projects' ? 'text-white' : 'text-slate-400'}`}
           >
             <ProjectIcon
               className={`w-5 h-5 ${pathname === '/projects' ? 'stroke-white' : 'stroke-slate-400'}`}
@@ -42,7 +42,7 @@ export const Header = () => {
         </Link>
         <Link href="/posts" aria-label="Go to posts page">
           <div
-            className={`flex items-center gap-1 ${pathname === '/posts' ? 'text-white' : 'text-slate-400'}`}
+            className={`flex items-center gap-1 cursor-pointer ${pathname === '/posts' ? 'text-white' : 'text-slate-400'}`}
           >
             <PostIcon
               className={`w-5 h-5 ${pathname === '/posts' ? 'stroke-white' : 'stroke-slate-400'}`}
