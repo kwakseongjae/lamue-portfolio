cdaaf4f2d31a695e9e9b185e8506794a65562e86
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-19 18:40:01 +0900

    feat: Enhance HeroSection with animated cursor and styled name display

--- src/widgets/home/ui/HeroSection.tsx
+++ src/widgets/home/ui/HeroSection.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { LocationIcon } from '@/shared/assets'
 import Image from 'next/image'
 import { ProfileImage } from '@/shared/assets'
+import Lottie from 'lottie-react'
+import { CursorLottie } from '@/shared/assets'
 
 const HeroSection = () => {
   return (
@@ -14,9 +18,24 @@ const HeroSection = () => {
           className="rounded-full w-25 aspect-square object-cover"
         />
         <div className="flex flex-col gap-2">
-          <div className="flex space-x-2 text-lg">
+          <div className="flex space-x-2 text-lg relative">
             <p>HI, I'M</p>
-            <p>KWAK SEONG JAE</p>
+            <p
+              className="border-b-2"
+              style={{
+                borderImage:
+                  'repeating-linear-gradient(to right, transparent 0%, transparent 2%, #E05634 2%, #E05634 6%) 1 0 100%',
+                borderImageSlice: '0 0 1 0',
+                borderStyle: 'solid',
+              }}
+            >
+              KWAK SEONG JAE
+            </p>
+            <Lottie
+              animationData={CursorLottie}
+              className="absolute -bottom-13 right-0 w-15 h-15"
+              loop={false}
+            />
           </div>
           <div className="flex space-x-2 items-center">
             <LocationIcon className="w-5 h-5" />
