4320e484554f161c4c6d06abcf29c7c22efdbafd
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-19 16:54:05 +0900

    feat: Import additional image and SVG assets in index.ts

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -1,4 +1,9 @@
+// 이미지 파일 임포트
+export { default as ProfileImage } from '/public/assets/image/fubao.jpg'
+
+// SVG 파일 임포트
 export { default as HomeIcon } from '/public/assets/svg/home-icon.svg'
 export { default as ProjectIcon } from '/public/assets/svg/folder-icon.svg'
 export { default as PostIcon } from '/public/assets/svg/blog-icon.svg'
 export { default as MailIcon } from '/public/assets/svg/mail-icon.svg'
+export { default as LocationIcon } from '/public/assets/svg/location-icon.svg'
