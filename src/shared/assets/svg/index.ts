536fb8b0b0480296944bdea4ffe937dadc714a33
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-19 16:18:36 +0900

    fix: Update SVG icon paths to use public directory

--- src/shared/assets/svg/index.ts
+++ src/shared/assets/svg/index.ts
@@ -1,4 +1,4 @@
-export { default as HomeIcon } from './home-icon.svg'
-export { default as ProjectIcon } from './folder-icon.svg'
-export { default as PostIcon } from './blog-icon.svg'
-export { default as MailIcon } from './mail-icon.svg'
+export { default as HomeIcon } from '/public/assets/svg/home-icon.svg'
+export { default as ProjectIcon } from '/public/assets/svg/folder-icon.svg'
+export { default as PostIcon } from '/public/assets/svg/blog-icon.svg'
+export { default as MailIcon } from '/public/assets/svg/mail-icon.svg'
