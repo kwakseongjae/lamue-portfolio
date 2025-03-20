0e9904bd0478c97f43d3f20e48c81226ac0756e0
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-20 21:03:55 +0900

    style: Update SectionContainer to use flex layout with gap

--- src/shared/ui/SectionContainer.tsx
+++ src/shared/ui/SectionContainer.tsx
@@ -1,3 +1,3 @@
 export default function SectionContainer({ children }: { children: React.ReactNode }) {
-  return <div className="px-8 py-6">{children}</div>
+  return <div className="px-8 py-6 flex flex-col gap-3">{children}</div>
 }
