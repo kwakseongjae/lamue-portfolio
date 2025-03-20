045988d72f1072253a2ad6d1c41c5ab600047fa3
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-20 22:21:49 +0900

    feat: Add ScrollToTopButton component and integrate it into the Home page

---
+++ src/components/ScrollToTopButton.tsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { ArrowIcon } from '@/shared/assets'
+
+export default function ScrollToTopButton() {
+  const handleScrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+
+  return (
+    <button
+      onClick={handleScrollToTop}
+      aria-label="Scroll to top"
+      className="cursor-pointer fixed bottom-[40px] right-8 bg-white hover:bg-gray-200 text-black w-[32px] h-[32px] flex items-center justify-center rounded-full shadow-lg"
+    >
+      <ArrowIcon className="w-[24px] h-[24px]" />
+    </button>
+  )
+}
