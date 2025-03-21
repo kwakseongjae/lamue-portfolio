42fc4214af17a4b3ec87a397de9f80d27ff5ab69
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-21 17:51:21 +0900

    chore: move ScrollToTopButton component to share/ui folder

--- src/shared/ui/ScrollToTopButton.tsx
+++ src/shared/ui/ScrollToTopButton.tsx
@@ -13,7 +13,12 @@ export default function ScrollToTopButton() {
       aria-label="Scroll to top"
       className="cursor-pointer fixed bottom-[40px] right-8 bg-white hover:bg-gray-200 text-black w-[32px] h-[32px] flex items-center justify-center rounded-full shadow-lg"
     >
-      <ArrowIcon className="w-[24px] h-[24px]" />
+      <ArrowIcon
+        className="w-[24px] h-[24px]"
+        fill="1E1E1E"
+        stroke="1E1E1E"
+        aria-label="Scroll to top"
+      />
     </button>
   )
 }
