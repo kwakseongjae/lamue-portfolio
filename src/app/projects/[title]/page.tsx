4b8d424d6057c4fcf44ad0f65c1d5410f58f5c99
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-23 21:36:03 +0900

    feat: Enhance ProjectDetail page with animated background and add BackButton component to page top

--- src/app/projects/[title]/page.tsx
+++ src/app/projects/[title]/page.tsx
@@ -1,14 +1,34 @@
 'use client'
 
-import { useParams } from 'next/navigation' // Use useParams instead of useRouter
+import BackButton from '@/shared/ui/BackButton'
+import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 const ProjectDetail = () => {
   const { title } = useParams()
+  const [animateBackground, setAnimateBackground] = useState(false)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setAnimateBackground(true)
+    }, 300)
+
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold">Project: {title}</h1>
-      <p className="mt-2">Details about the project will go here.</p>
+    <div
+      className={`w-full min-h-screen px-8 py-4 transition-colors duration-1000 ${
+        animateBackground
+          ? 'bg-radial-[at_50%_0%] from-[#244b6f] via-[#1c2d3e] to-[#000000]'
+          : 'bg-bg-primary'
+      }`}
+    >
+      <BackButton />
+      <div className="p-4">
+        <h1 className="text-3xl font-bold">Project: {title}</h1>
+        <p className="mt-2">Details about the project will go here.</p>
+      </div>
     </div>
   )
 }
