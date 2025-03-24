4d8aeaf8816fa2b59440e72a53433db9f4a7833b
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-24 18:18:22 +0900

    style: Update ProjectList component to set width to 2/3

--- src/shared/ui/ProjectList.tsx
+++ src/shared/ui/ProjectList.tsx
@@ -1,3 +1,3 @@
 export default function ProjectList({ children }: { children: React.ReactNode }) {
-  return <div className="flex flex-col gap-8">{children}</div>
+  return <div className="flex flex-col w-2/3 gap-8">{children}</div>
 }
