f7ac128f9e363cb97f46d6d17fcbc1e5386a2c3e
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-24 16:50:05 +0900

    style: Update BackButton component to include background color for improved visibility

--- src/shared/ui/BackButton.tsx
+++ src/shared/ui/BackButton.tsx
@@ -14,7 +14,7 @@ export default function BackButton() {
       className="text-xl font-semibold cursor-pointer"
       aria-label="Go back"
     >
-      <div className="relative flex items-center gap-2 border border-slate-800 rounded-3xl px-4 py-2">
+      <div className="relative flex items-center gap-2 border border-slate-800 rounded-3xl px-4 py-2 bg-gray-800">
         <ArrowIcon className="w-5 h-5 rotate-270" fill="#fafaf8" />
         <span>Back</span>
       </div>
