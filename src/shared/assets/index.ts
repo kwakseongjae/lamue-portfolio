e1e8b45075abe71d516ea71a6dfc6e0572c7ca56
kwakseongjae <gkffhdnls13@gmail.com>
2025-03-24 17:48:19 +0900

    feat: Add hamburger menu icon SVG and export it in assets index

--- src/shared/assets/index.ts
+++ src/shared/assets/index.ts
@@ -27,6 +27,7 @@ export { default as JavaIcon } from '/public/assets/svg/skills/java-icon.svg'
 export { default as SpringBootIcon } from '/public/assets/svg/skills/spring-boot-icon.svg'
 export { default as SpringSecurityIcon } from '/public/assets/svg/skills/spring-security-icon.svg'
 export { default as INVY } from '/public/assets/svg/INVY.svg'
+export { default as MenuIcon } from '/public/assets/svg/hamburger-menu-icon.svg'
 
 // Lottie 파일 임포트
 export { default as CursorLottie } from '/public/assets/lottie/cursor-lottie.json'
